d3e1f3a11a011a08a43ef229630e0ff3bf5981ec
Cleiton Marques <cleiton.p.marques@gmail.com>
2021-08-27 17:52:41 -0300

    Updated local database

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 
 module.exports = {
     development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOSTNAME,
-        port: process.env.DB_PORT,
+        username: 'root',
+        password: '100grilo',
+        database: 'sgLegis',
+        host: 'localhost',
+        port: 3306,
         dialect: 'mysql',
     },
     production: {
@@ -16,10 +16,10 @@ module.exports = {
         host: process.env.DB_HOSTNAME,
         port: process.env.DB_PORT,
         dialect: 'mysql',
-        dialectOptions: {
-            ssl: {
-                ca: fs.readFileSync(__dirname + '/ca-certificate.crt')
-            }
-        }
+        // dialectOptions: {
+        //     ssl: {
+        //         ca: fs.readFileSync(__dirname + '/ca-certificate.crt')
+        //     }
+        // }
     }
 };
