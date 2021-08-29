0c985afafd18d024e58ff04e8bbf146e884c1c68
Cleiton Marques <cleiton.p.marques@gmail.com>
2021-08-29 17:10:41 -0300

    Teste

--- src/routes/unitContactRoutes.js
+++ src/routes/unitContactRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/contactunitController');
+const controller = require('../controllers/contactUnitController');
 
 router.post('/', controller.post);
 router.get('/', controller.getAll);
