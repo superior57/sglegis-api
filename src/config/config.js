4e0d5666cd571b6804bf6737ce1b4f943a16c6b4
Cleiton Marques <cleiton.p.marques@gmail.com>
2021-08-29 18:01:45 -0300

    Better database connection

--- src/config/config.js
+++ src/config/config.js
@@ -10,12 +10,8 @@ module.exports = {
         dialect: 'mysql',
     },
     production: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOSTNAME,
-        port: process.env.DB_PORT,
         dialect: 'mysql',
+        uri: process.env.DATABASE_URL
         // dialectOptions: {
         //     ssl: {
         //         ca: fs.readFileSync(__dirname + '/ca-certificate.crt')
