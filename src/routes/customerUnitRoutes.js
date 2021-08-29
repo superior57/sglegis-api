2670515db14cb21a6cc7168e774fd2e2db5cb43b
Cleiton Marques <cleiton.p.marques@gmail.com>
2021-08-29 17:13:59 -0300

    Teste

--- src/routes/customerUnitRoutes.js
+++ src/routes/customerUnitRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/customerUnitController');
 const controllerAreaAspect = require('../controllers/unitAreaAspectController');
 const controllerResponsible = require('../controllers/unitAspectResponsibleController');
-const controllerResponsibleAspect = require('../controllers/Porra');
+const controllerResponsibleAspect = require('../controllers/responsibleAspectController');
 
 router.post('/', controller.post);
 router.get('/', controller.getAll);
