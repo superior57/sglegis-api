80e5c623d6095dcd186d8e895cc7905c4d6ae4ed
Cleiton Marques <cleiton.p.marques@gmail.com>
2021-09-05 11:57:48 -0300

    Formating stuff on model + uniq added

--- src/models/audits.js
+++ src/models/audits.js
@@ -5,15 +5,10 @@ module.exports = function (sequelize, DataTypes) {
     audit_id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
     unit_id: { type: DataTypes.INTEGER, allowNull: false, comment: 'The unit' },
     item_area_aspect_id: { type: DataTypes.INTEGER, allowNull: false, comment: 'The key matched document_id x area_id and aspect_id' },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: true
-    }
+    createdAt: { type: DataTypes.DATE, allowNull: false },
+    updatedAt: { type: DataTypes.DATE, allowNull: true }
 
+  }, { indexes: [{ unique: true, name: 'uniq_audit_per_unit', fields: ['item_area_aspect_id', 'unit_id'] }]
   }, {
     tableName: 'audits'
   });
