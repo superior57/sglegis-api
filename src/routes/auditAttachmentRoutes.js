ae69bdce558adf0bf450a333eae123dfd1649ac9
Cleiton Marques <cleiton.p.marques@gmail.com>
2021-09-20 10:01:42 -0300

    Added audit attachments controller and routes

---
+++ src/routes/auditAttachmentRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/auditAttachmentController');
+const file = require('../middleware/file');
+
+file.setFolder('audits');
+
+router.post('/', file.fileUploader.single('attachment_file'),  controller.post);
+router.get('/', controller.getAll);
+router.get('/:id', controller.get);
+router.get('/attachments/:id', controller.getAttachments);
+router.delete('/:id', controller.delete);
+
+module.exports = router;
