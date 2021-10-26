377042aee9b7c37de7372331192506b2ce94a3d6
Cleiton Marques <cleiton.p.marques@gmail.com>
2021-10-26 19:37:27 +0100

    Increased size of document_number field and added some comments

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
 
 module.exports = {
-    development: {
+    local: {
         username: 'root',
         password: '100grilo',
         database: 'sgLegis',
         host: 'localhost',
         port: 3306,
         dialect: 'mysql',
+    },
+    development: {
+        dialect: 'mysql',
+        port: process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: process.env.DATABASE_NAME,
+        host: process.env.DATABASE_HOST,
     },
     production: {
         dialect: 'mysql',
--- src/models/documents.js
+++ src/models/documents.js
@@ -2,12 +2,12 @@
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define('documents', {
         document_id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement : true },
-        document_scope_id: { type: DataTypes.INTEGER, allowNull: false },
-        document_type: { type: DataTypes.TEXT, allowNull: true },
-        document_number: { type: DataTypes.STRING(50), allowNull: true, defaultValue: 'S/N' },
-        document_date: { type: DataTypes.DATE, allowNull: false },
-        document_status_id: { type: DataTypes.INTEGER, allowNull: false },
-        document_summary: { type: DataTypes.STRING(2000), allowNull: false },
+        document_scope_id: { type: DataTypes.INTEGER, allowNull: false, comment: "The scope this document can range (global, country, state or city) - (document_scopes model)" },
+        document_type: { type: DataTypes.TEXT, allowNull: true, comment: "The type of document. Commonly is a law or standard rule (norm)." },
+        document_number: { type: DataTypes.STRING(100), allowNull: true, defaultValue: 'S/N', comment: "A control number of document" },
+        document_date: { type: DataTypes.DATE, allowNull: false, comment: "The date that this document was released" },
+        document_status_id: { type: DataTypes.INTEGER, allowNull: false, comment: "The state id that this document applies (state model)" },
+        document_summary: { type: DataTypes.STRING(5000), allowNull: false, comment: "A small text that summarize the document text" },
         document_state_id: { type: DataTypes.INTEGER, allowNull: true, comment: "The region of country document is valid (filled when scope = state)" },
         document_city_id: { type: DataTypes.INTEGER, allowNull: true, comment: "The city of region (state) document is valid (filled when scope = city)" },
         document_privacy_type: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0, comment: "0 = public (available for all customers) / 1 = private (need to check table document_privacy_customers)" },
