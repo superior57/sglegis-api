bafa8df56a29420589e3dd5e8a545af7f2c03df8
Cleiton Marques <cleiton.p.marques@gmail.com>
2021-11-02 23:02:41 +0000

    Updated the queries to use parameters

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,7 +1,8 @@
-const { document_items } = require('../models');
+
 const base = require('./baseController');
 const db = require('../models/index');
 const sequelize = require('sequelize');
+const options = require('./queryoptions');
 
 exports.getCumulativeCompliance = (req, res, next) => {
     // - 
@@ -20,6 +21,16 @@ exports.getCumulativeCompliance = (req, res, next) => {
 }
 
 exports.getPraticalOrderCompliance = (req, res, next) => {
+    //add mandatory filter
+    req.query.audit_practical_order = '2';
+
+    const query = {};
+    Object.keys(req.query).forEach(key => {
+        if (req.query[key] !== "" && req.query[key] != 'null' && req.query[key] != null ) {
+            query[key] = req.query[key]
+        }
+    })
+
     let sql = `
     select
            count(1) as _count,
@@ -27,39 +38,75 @@ exports.getPraticalOrderCompliance = (req, res, next) => {
            getConformity(ai.audit_conformity) as _labels
     from audits a
     inner join audit_items ai on a.audit_id = ai.audits_audit_id
-    where audit_practical_order = 2 /* PRATICAL ORDER = SIM*/
-    group by ai.audit_conformity
+    inner join customers_units cu on a.unit_id = cu.customer_unit_id
+    inner join customers_groups cg on cu.customer_id = cg.customer_group_id
+    inner join items_areas_aspects iaa on a.item_area_aspect_id = iaa.item_area_aspect_id
     `
 
-    // req.query.fields = [{ fields: 'document_id', ops: 'eq', values: req.params.id }]
+    for (let i = 0; i < Object.keys(query).length; i ++) {
+        const key = Object.keys(query)[i];
+        if (i == 0) sql += ` WHERE `;
+            sql += `${key} = '${query[key]}'`;
+        if (i < Object.keys(query).length - 1) sql += ` AND `;           
+    }
+
+    sql += ` group by ai.audit_conformity;`;
+
     base.rawquery(sql, req, res, next);
 }
-
 
 exports.getResponsibleByConformity = (req, res, next) => {
     //     -- responsável por não conformidade (%)
 
-    let sql = ` 
+   //add mandatory filter
+    req.query.audit_practical_order = '2'; //pratical order = SIM
+    req.query.audit_conformity = '4'; //conformidade = NÃO
+
+   const query = {};
+   Object.keys(req.query).forEach(key => {
+       if (req.query[key] !== "" && req.query[key] != 'null' && req.query[key] != null ) {
+           query[key] = req.query[key]
+       }
+   })
+
+   let sql = `
     select
         count(1) as _count,
         count(1) * 100.0 / sum(count(1)) over() as _percentage,
-        COALESCE(uar.unit_aspect_responsible_name, NULL, 'SEM RESPONSÁVEL') as _labels
+        uar.unit_aspect_responsible_name as _labels
     from audits a
     inner join audit_items ai on a.audit_id = ai.audits_audit_id
-    left join items_areas_aspects iaa on a.item_area_aspect_id = iaa.item_area_aspect_id
-    left join responsibles_aspects ra on iaa.area_aspect_id = ra.area_aspect_id
-    left join units_aspects_responsibles uar on ra.unit_aspect_responsible_id = uar.unit_aspect_responsible_id
-    where ai.audit_practical_order = 2 and ai.audit_conformity = 4 /* Pratical order = SIM e Conformidade = NAO */
-            and iaa.area_id = 3
-    group by
-            uar.unit_aspect_responsible_name
-    `
-    // req.query.fields = [{ fields: 'document_id', ops: 'eq', values: req.params.id }]
-    base.rawquery(sql, req, res, next);
+    inner join items_areas_aspects iaa on a.item_area_aspect_id = iaa.item_area_aspect_id
+    inner join responsibles_aspects ra on iaa.area_aspect_id = ra.area_aspect_id
+    inner join units_aspects_responsibles uar on ra.unit_aspect_responsible_id = uar.unit_aspect_responsible_id
+    inner join customers_units cu on a.unit_id = cu.customer_unit_id
+    inner join customers_groups cg on cu.customer_id = cg.customer_group_id
+   `
+
+   for (let i = 0; i < Object.keys(query).length; i ++) {
+       const key = Object.keys(query)[i];
+       if (i == 0) sql += ` WHERE `;
+           sql += `${key} = '${query[key]}'`;
+       if (i < Object.keys(query).length - 1) sql += ` AND `;           
+   }
+
+   sql += ` group by uar.unit_aspect_responsible_name;`;
+
+    console.log(sql);
+    
+   base.rawquery(sql, req, res, next);
+
 };
 
 exports.getGeneralCompliance = (req, res, next) => {
     // - compliance geral (aplicável = SIM e compliance SIM / NAO) (%)
+
+    const query = {};
+    Object.keys(req.query).forEach(key => {
+        if (req.query[key] !== "" && req.query[key] != 'null' && req.query[key] != null ) {
+            query[key] = req.query[key]
+        }
+    })
 
     let sql = ` 
     SELECT
@@ -74,7 +121,7 @@ exports.getGeneralCompliance = (req, res, next) => {
     INNER JOIN (
             SELECT
                     cu.customer_id, cu.customer_unit_id, cu.customer_unit_uf_id, cu.customer_unit_city_id,
-                    uaa.area_id, uaa.area_aspect_id, cu.customer_unit_name, cs.customer_business_name, cs.customer_group_id,
+                    uaa.area_id as areas_area_id, uaa.area_aspect_id, cu.customer_unit_name, cs.customer_business_name, cs.customer_group_id,
                     uar.unit_aspect_responsible_name, uar.unit_aspect_responsible_email
             FROM customers_units cu
             INNER JOIN units_areas_aspects uaa ON cu.customer_unit_id = uaa.customer_unit_id
@@ -91,17 +138,37 @@ exports.getGeneralCompliance = (req, res, next) => {
 				 GROUP BY uar.customer_unit_id, ra.area_aspect_id
             ) uar ON uar.customer_unit_id = cu.customer_unit_id and uar.area_aspect_id = uaa.area_aspect_id
         ) unit_data ON
-        (d.document_scope_id = 4 /*MUNICIPAL*/ AND d.document_city_id = unit_data.customer_unit_city_id AND unit_data.area_aspect_id = iaa.area_aspect_id AND unit_data.area_id = iaa.area_id) OR
-        (d.document_scope_id = 3 /*ESTADUAL*/ AND d.document_state_id = unit_data.customer_unit_uf_id AND unit_data.area_aspect_id = iaa.area_aspect_id AND unit_data.area_id = iaa.area_id) OR
-        ((d.document_scope_id = 1 OR d.document_scope_id = 2)/*FEDERAL ou GLOBAL*/ AND iaa.area_aspect_id = unit_data.area_aspect_id AND unit_data.area_id = iaa.area_id)
-    GROUP BY unit_aspect_responsible_name
+        (d.document_scope_id = 4 /*MUNICIPAL*/ AND d.document_city_id = unit_data.customer_unit_city_id AND unit_data.area_aspect_id = iaa.area_aspect_id AND unit_data.areas_area_id = iaa.area_id) OR
+        (d.document_scope_id = 3 /*ESTADUAL*/ AND d.document_state_id = unit_data.customer_unit_uf_id AND unit_data.area_aspect_id = iaa.area_aspect_id AND unit_data.areas_area_id = iaa.area_id) OR
+        ((d.document_scope_id = 1 OR d.document_scope_id = 2)/*FEDERAL ou GLOBAL*/ AND iaa.area_aspect_id = unit_data.area_aspect_id AND unit_data.areas_area_id = iaa.area_id)
     `
+
+    for (let i = 0; i < Object.keys(query).length; i ++) {
+        const key = Object.keys(query)[i];
+        if (i == 0) sql += ` WHERE `;
+            sql += `${key} = '${query[key]}'`;
+        if (i < Object.keys(query).length - 1) sql += ` AND `;           
+    }
+ 
+    sql += ` GROUP BY unit_aspect_responsible_name;`;
+ 
+    console.log(sql);
+     
     // req.query.fields = [{ fields: 'document_id', ops: 'eq', values: req.params.id }]
     base.rawquery(sql, req, res, next);
 };
 
 exports.getResponsibleByAspect = (req, res, next) => {
     // -- responsável por aspecto (%)
+    req.query.audit_practical_order = '2'; //pratical order = SIM
+    req.query.audit_conformity = '4'; //conformidade = NÃO
+
+    const query = {};
+    Object.keys(req.query).forEach(key => {
+        if (req.query[key] !== "" && req.query[key] != 'null' && req.query[key] != null ) {
+            query[key] = req.query[key]
+        }
+    })
 
     let sql = ` 
     select
@@ -112,11 +179,20 @@ exports.getResponsibleByAspect = (req, res, next) => {
     inner join audit_items ai on a.audit_id = ai.audits_audit_id
     inner join items_areas_aspects iaa on a.item_area_aspect_id = iaa.item_area_aspect_id
     inner join areas_aspects aa on iaa.area_aspect_id = aa.area_aspect_id
-    where ai.audit_practical_order = 2 and ai.audit_conformity = 4 /* Pratical order = SIM e Conformidade = NAO */
-            and aa.area_id = 3
-    group by
-            iaa.area_aspect_id
+    inner join customers_units cu on a.unit_id = cu.customer_unit_id
+    inner join customers_groups cg on cu.customer_id = cg.customer_group_id
     `
-    // req.query.fields = [{ fields: 'document_id', ops: 'eq', values: req.params.id }]
+
+    for (let i = 0; i < Object.keys(query).length; i ++) {
+        const key = Object.keys(query)[i];
+        if (i == 0) sql += ` WHERE `;
+            sql += `${key} = '${query[key]}'`;
+        if (i < Object.keys(query).length - 1) sql += ` AND `;           
+    }
+ 
+    sql += ` group by aa.area_aspect_name;`;
+ 
+    console.log(sql);
+     
     base.rawquery(sql, req, res, next);
 };
